35ef3d3f6a9e953332db7ddc3fe52a866b6d42b8
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-04-13 00:30:04 +0200

    Added DataAPI and variables for window and ball in Data layer

---
+++ Data/AbstractDataAPI.cs
@@ -0,0 +1,14 @@
+﻿namespace Data
+{
+    public abstract class AbstractDataAPI
+    {
+        public abstract int WindowHeight { get; }
+        public abstract int WindowWidth { get; }
+        public abstract int BallDiameter { get; }
+
+        public static AbstractDataAPI CreateInstance()
+        {
+            return new Data();
+        }
+    }
+}
---
+++ Data/Data.cs
@@ -0,0 +1,11 @@
+﻿namespace Data
+{
+    internal class Data : AbstractDataAPI
+    {
+        public override int WindowHeight => 478;
+
+        public override int WindowWidth => 798;
+
+        public override int BallDiameter => 20;
+    }
+}
