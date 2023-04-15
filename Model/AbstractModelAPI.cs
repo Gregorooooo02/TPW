77b4c6b32daaf918b628b04a973f136194085bda
Jedrzej Kostyk <jedrek2504@github.com>
2023-04-15 15:47:39 +0200

    Finished Presentation/Model -> created AbstractModelAPI and Model class that implements API

---
+++ Model/AbstractModelAPI.cs
@@ -0,0 +1,20 @@
+﻿using Logic;
+
+namespace Model
+{
+    public abstract class AbstractModelAPI : IObserver<IEnumerable<Ball>>, IObservable<IEnumerable<BallModel>>
+    {
+        public abstract void SpawnBalls(int numberOfBalls);
+        public abstract void Start();
+        public abstract void Stop();
+        public abstract void OnCompleted();
+        public abstract void OnError(Exception error);
+        public abstract void OnNext(IEnumerable<Ball> balls);
+        public abstract IDisposable Subscribe(IObserver<IEnumerable<BallModel>> observer);
+
+        public static AbstractModelAPI CreateInstance(AbstractLogicAPI? logic = default)
+        {
+            return new Model(logic ?? AbstractLogicAPI.CreateInstance());
+        }
+    }
+}
