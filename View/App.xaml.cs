bab6233ab7435c6f8567018321eb009f744b441f
Jedrzej Kostyk <jedrek2504@github.com>
2023-04-15 23:27:00 +0200

    Presentation/View finished -> added GUI and connected it to Presentation/ViewModel

--- View/App.xaml.cs
+++ View/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ViewModel;
 using System.Windows;
 
 namespace View
@@ -13,5 +8,15 @@ namespace View
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
---
+++ View/SimulationWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SimulationWindow.xaml
+    /// </summary>
+    public partial class SimulationWindow : UserControl
+    {
+        public SimulationWindow() : base()
+        {
+            InitializeComponent();
+        }
+    }
+}
