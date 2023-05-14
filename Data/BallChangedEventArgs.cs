1693704dbbf5828b48d0672212c97cc4f37ba5c7
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-05-14 20:08:28 +0200

    Added BallChangedEventArgs class

---
+++ Data/BallChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    public class BallChangedEventArgs : EventArgs
+    {
+        public IBallData Ball { get; set; }
+
+        public BallChangedEventArgs(IBallData ball)
+        {
+            Ball = ball;
+        }
+    }
+
+}
