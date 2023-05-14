c00733de967c462b5df65be3b0d0baeac4464ca7
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-05-14 19:50:54 +0200

    Created IBallData interface in Data Layer

---
+++ Data/IBallData.cs
@@ -0,0 +1,14 @@
+﻿namespace Data
+{
+    public interface IBallData : IObservable<IBallData>
+    {
+        int Diameter { get; init; }
+        float SpeedX { get; }
+        float SpeedY { get; }
+        float PositionX { get; }
+        float PositionY { get; }
+
+        Task SetSpeed(float speedX, float speedY);
+        Task Move(float moveX, float moveY);
+    }
+}
