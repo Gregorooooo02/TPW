3521837d1e588ba2d1e602d15f6ff336d4a47bb2
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-05-28 22:11:54 +0200

    Implemented ViewModelAPI

--- ViewModel/AbstractViewModelAPI.cs
+++ ViewModel/AbstractViewModelAPI.cs
@@ -1,31 +1,33 @@
 ﻿using Model;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace ViewModel
 {
-    public abstract class ViewModel : INotifyPropertyChanged
+    public abstract class AbstractViewModelAPI : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public abstract ICommand StartSimInput { get; }
+        public abstract ICommand StopSimInput { get; }
+        public abstract ICommand SpawnBallInput { get; }
+        public abstract ObservableCollection<object> _balls { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName)
+        public abstract void StartSimulation();
+        public abstract void StopSimulation();
+        public abstract void SpawnBall();
+        public abstract ObservableCollection<object> GetBalls();
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected bool SetField<T>(ref T field, T value, IValidator<T> validator, T def, [CallerMemberName] string propertyName = "")
+        public static AbstractViewModelAPI CreateInstance(int windowHeight, int windowWidth)
         {
-            if (validator.IsInvalid(value)) value = def;
-            return SetField(ref field, value, propertyName);
-        }
-
-        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-            field = value;
-            OnPropertyChanged(propertyName);
-            return true;
+            return new ViewModel(windowHeight, windowWidth);
         }
     }
-
 }
