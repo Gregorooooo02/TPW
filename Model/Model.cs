6edb4628efb25c91a7c09e97718736b36ca62ab7
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-05-28 21:57:15 +0200

    Refactored Model Layer - implemented ModelAPI in Model Class

--- Model/Model.cs
+++ Model/Model.cs
@@ -1,6 +1,42 @@
 ﻿using Logic;
+using System.Collections.ObjectModel;
 
 namespace Model
 {
-    
+    internal class Model : AbstractModelAPI
+    {
+        private AbstractBallAPI _ballAPI;
+
+        public Model (AbstractBallAPI ballAPI)
+        {
+            _ballAPI = ballAPI;
+        }
+
+        public override ObservableCollection<object> GetBalls()
+        {
+            ObservableCollection<object> _balls = new ObservableCollection<object>();
+
+            foreach (object _ball in _ballAPI.BallsList)
+            {
+                _balls.Add(_ball);
+            }
+
+            return _balls;
+        }
+
+        public override void SpawnBall()
+        {
+            _ballAPI.SpawnBall();
+        }
+
+        public override void StartSimulation()
+        {
+            _ballAPI.StartSimulation();
+        }
+
+        public override void StopSimulation()
+        {
+            _ballAPI.StopSimulation();
+        }
+    }
 }
